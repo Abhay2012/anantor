2e7b920cb75a671c73ec488f01ae0406608c1815
Abhay Sehgal <abhay.sehgal2012@gmail.com>
2025-10-04 22:49:48 +0530

    favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,13 @@ export const metadata = {
   title: 'Anantor',
   description: 'Discover the cosmos within',
   metadataBase: new URL('https://www.anantor.com'),
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon.svg', type: 'image/svg+xml' },
+    ],
+    shortcut: ['/favicon.ico'],
+  },
 }
 
 import './globals.css'

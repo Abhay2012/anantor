61841060a5805fc8bd5c8ec52805a5ac8b56bb02
Abhay Sehgal <abhay.sehgal2012@gmail.com>
2025-11-15 09:22:48 +0530

    analytics added

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,13 +10,16 @@ export const metadata = {
     shortcut: ['/favicon.ico'],
   },
 }
-
+import { Analytics } from '@vercel/analytics/next';
 import './globals.css'
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
